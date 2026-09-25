cb7cdaaac8fc2000395fd67d06e091f4cb7f297a
Rudi Welter <rwdevops999@gmail.com>
2026-09-25 07:46:05 +0200

    Update Registry.java
    solved typo

--- src/main/java/util/Registry.java
+++ src/main/java/util/Registry.java
@@ -8,22 +8,22 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class Registry {
-    private final static Map<String, BiConsumer<Node, Object>> consumerregistry = new HashMap<>();
-    private final static Map<String, Node> noderegistry = new HashMap<>();
+    private final static Map<String, BiConsumer<Node, Object>> consumerRegistry = new HashMap<>();
+    private final static Map<String, Node> nodeRegistry = new HashMap<>();
 
     public static void register(String name, Node node, BiConsumer<Node, Object> consumer) {
-        noderegistry.put(name, node);
-        consumerregistry.put(name, consumer);
+        nodeRegistry.put(name, node);
+        consumerRegistry.put(name, consumer);
     }
 
     public static void unregister(String name) {
-        noderegistry.remove(name);
-        consumerregistry.remove(name);
+        nodeRegistry.remove(name);
+        consumerRegistry.remove(name);
     }
 
     public static void publish(String name, Object data) {
-        if (noderegistry.containsKey(name)) {
-            consumerregistry.get(name).accept(noderegistry.get(name), data);
+        if (nodeRegistry.containsKey(name)) {
+            consumerRegistry.get(name).accept(nodeRegistry.get(name), data);
         }
     }
 }
